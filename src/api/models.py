02631f9b47047bf213fe70f6f5d1a0445392ceaa
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2023-12-02 23:12:39 +0000

    Creando los endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -3,19 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique= True) 
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique= True)
+    user_name = db.Column(db.String(50), unique= True)
     email = db.Column(db.String(120), unique=True, nullable=False)    
+    address = db.Column(db.String(80), unique=False, nullable=False)
+    phone = db.Column(db.String(80), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
         return {
-            "id": self.id,
+            "user_id": self.user_id,
+            "name": self.name,
+            "user_name": self.user_name,
             "email": self.email,
-            "password": self.password
+            "address": self.address,
+            "phone": self.phone,
+            "is_admin": self.is_admin
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,20 +5,92 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from models import db, User
+from flask_jwt_extend import JWTManager, create_access_token, jwt_required, get_jwt_identy
 import json
+import bcrypt
+
+
 
 api = Blueprint('api', __name__)
+
+api.config["JWT_SECRET_KEY"] = "jwt_password" 
+jwt = JWTManager(api)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route('/singup', methods=['POST'])
+def create_user():
+    body = json.loads(request.data)
+    # body = request.json()
+    raw_password = request.json.get('password')
+    password_hash = bcrypt.generate_password_hash(raw_password).decode('utf-8')
+    new_user = User(
+        user_id = body['user_id'],
+        name = body["name"],
+        user_name = body["user_name"],
+        email = body["email"],
+        address = body["address"],
+        phone = body["phone"],
+        password = body[{password_hash}],        
+        is_admin = body["is_admin"]
+    )    
+    
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "user created succesfull", "user_added": new_user }), 200
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if not email or not password:
+        return jsonify({"msg": "Bad email or password"}), 401
+    existing_user = User.query.filter_by(email = email).firt()
+    if not existing_user:
+        return jsonify({"msg": "Email not found"})
+    password_hash = existing_user.password
+    true_or_fale = bcrypt.check_password_hash(password_hash, password)
+    if not true_or_fale:
+        return jsonify({"msg": "Wrong password "}), 400
+    
+    user_id = existing_user.id
+    access_token = create_access_token(identity = user_id)
+    return jsonify({"access_token": access_token, "msg": "Access Success"}), 200
+    
 
-    return jsonify(response_body), 200
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def get_user():
+    currents_user_id = get_jwt_identy()
+    if currents_user_id:
+        users = User.query.all()
+        users_list = []
+        for user in users:
+            users_dict = {
+                'id': 'user_id',
+                'email': 'user.email'
+            }
+            users_list.append(users_dict)
+        return jsonify(users_list), 200
+    else:
+        return {"Error": "Token inválido o no proporcionado"}, 401
+    return jsonify({"msg": "It works"})
+
+
+@api.route('/user', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    if len(users) < 1:
+        return jsonify({"msg": "not found"}), 404
+    serialized_users = [x.serialize() for x in users]
+    return serialized_users, 200
+
+@api.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"msg": f"User with id {user_id} not found"}), 404
+    serialized_user = user.serielized()
+    return serialized_user , 200
